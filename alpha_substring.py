fc08dad9d67965ec9ee3a4b461dbb74439a56614
Josh Branham <josh.php@gmail.com>
2017-06-09 14:06:15 -0400

    Uploading files

--- alpha_substring.py
+++ alpha_substring.py
@@ -1,11 +1,9 @@
-try = s[0]
-best = ''
-
-for n in range(1, len(s)):
-    if len(try) > len(best):
-        best = try
-    if s[n] >= s[n-1]:
-        try = try + s[n]
-    else:
-        try = s[n]
-print('Longest substring in alphabetical order is: ' + best)
+maxSub, currentSub, previousChar = '', '', ''
+for char in s:
+    if char >= previousChar:
+        currentSub = currentSub + char
+        if len(currentSub) > len(maxSub):
+            maxSub = currentSub
+    else: currentSub = char
+    previousChar = char
+print(maxSub)
